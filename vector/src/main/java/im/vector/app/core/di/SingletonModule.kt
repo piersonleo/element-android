bafafed602f464725e6ff1efb81fea050318500a
Benoit Marty <benoit@matrix.org>
2022-05-02 11:58:48 +0200

    Make the constructor public

--- vector/src/main/java/im/vector/app/core/di/SingletonModule.kt
+++ vector/src/main/java/im/vector/app/core/di/SingletonModule.kt
@@ -130,7 +130,7 @@ object VectorStaticModule {
     @Provides
     @Singleton
     fun providesMatrix(context: Context, configuration: MatrixConfiguration): Matrix {
-        return Matrix.createInstance(context, configuration)
+        return Matrix(context, configuration)
     }
 
     @Provides
