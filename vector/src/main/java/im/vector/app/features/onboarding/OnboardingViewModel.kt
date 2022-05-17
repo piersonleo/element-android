8864eb0e6c290a853371bd05265ee41d063c951d
Adam Brown <adampsbrown@gmail.com>
2022-05-17 10:31:32 +0100

    renaming comparator to only specify the matrix.org domain

--- vector/src/main/java/im/vector/app/features/onboarding/OnboardingViewModel.kt
+++ vector/src/main/java/im/vector/app/features/onboarding/OnboardingViewModel.kt
@@ -43,7 +43,7 @@ import im.vector.app.features.login.ReAuthHelper
 import im.vector.app.features.login.ServerType
 import im.vector.app.features.login.SignMode
 import im.vector.app.features.onboarding.StartAuthenticationFlowUseCase.StartAuthenticationResult
-import im.vector.app.features.onboarding.ftueauth.MatrixOrgMissingRegistrationStagesComparator
+import im.vector.app.features.onboarding.ftueauth.MatrixOrgRegistrationStagesComparator
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
@@ -297,7 +297,7 @@ class OnboardingViewModel @AssistedInject constructor(
         withState { state ->
             val orderedResult = when {
                 state.hasSelectedMatrixOrg() && vectorFeatures.isOnboardingCombinedRegisterEnabled() -> flowResult.copy(
-                        missingStages = flowResult.missingStages.sortedWith(MatrixOrgMissingRegistrationStagesComparator())
+                        missingStages = flowResult.missingStages.sortedWith(MatrixOrgRegistrationStagesComparator())
                 )
                 else                                                                                 -> flowResult
             }
