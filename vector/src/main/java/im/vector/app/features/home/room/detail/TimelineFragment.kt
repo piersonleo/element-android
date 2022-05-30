af3325ffeb52cbdf663ea98c062a806e50563013
Adam Brown <adampsbrown@gmail.com>
2022-05-30 10:38:42 +0100

    splitting url detection condition into separate branches

--- vector/src/main/java/im/vector/app/features/home/room/detail/TimelineFragment.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/TimelineFragment.kt
@@ -1927,10 +1927,13 @@ class TimelineFragment @Inject constructor(
                     })
             if (!isManaged) {
                 when {
-                    url.containsRtLOverride() || (title.isValidUrl() && url.isValidUrl() && URL(title).host != URL(url).host) -> {
+                    url.containsRtLOverride()                                                  -> {
                         displayUrlConfirmationDialog(title.ensureEndsLeftToRight(), url.filterDirectionOverrides())
                     }
-                    else                                                                                                      -> {
+                    title.isValidUrl() && url.isValidUrl() && URL(title).host != URL(url).host -> {
+                        displayUrlConfirmationDialog(title.ensureEndsLeftToRight(), url)
+                    }
+                    else                                                                       -> {
                         openUrlInExternalBrowser(requireContext(), url)
                     }
                 }
