4d87a38a0dd35c47af38cdbd3355b160ad76bf94
piersonleo <pierson.leo@vcard.com>
2022-06-06 13:40:22 +0700

    update(home): cancel verification alert on new login

--- vector/src/main/java/im/vector/app/features/home/HomeActivity.kt
+++ vector/src/main/java/im/vector/app/features/home/HomeActivity.kt
@@ -433,6 +433,7 @@ class HomeActivity :
                 it.navigator.requestSelfSessionVerification(it)
             }
         }
+        popupAlertManager.cancelAlert("upgradeSecurity")
     }
 
     private fun handlePromptToEnablePush() {
