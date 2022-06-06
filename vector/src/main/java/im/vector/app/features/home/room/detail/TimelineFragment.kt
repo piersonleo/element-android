5e4c6e833dde5c45b554eeba5f451484edce4db9
piersonleo <pierson.leo@vcard.com>
2022-06-06 17:16:50 +0700

    fix rebase conflicts between base vchat and v1.4.18

--- vector/src/main/java/im/vector/app/features/home/room/detail/TimelineFragment.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/TimelineFragment.kt
@@ -2494,8 +2494,7 @@ class TimelineFragment @Inject constructor(
                     displayName = timelineViewModel.getRoomSummary()?.displayName,
                     avatarUrl = timelineViewModel.getRoomSummary()?.avatarUrl,
                     roomEncryptionTrustLevel = timelineViewModel.getRoomSummary()?.roomEncryptionTrustLevel,
-                    rootThreadEventId = rootThreadEventId,
-                    showKeyboard = showKeyboard
+                    rootThreadEventId = rootThreadEventId
             )
             navigator.openThread(it, roomThreadDetailArgs)
         }
