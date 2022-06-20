315293681b500f6a67c24cdc6b7a6fe80b310e2b
piersonleo <pierson.leo@vcard.com>
2022-06-20 14:23:28 +0700

    update(vchat utils): update url and add asset util

--- vector/src/main/java/com/vcard/vchat/utils/Utils.kt
+++ vector/src/main/java/com/vcard/vchat/utils/Utils.kt
@@ -1,5 +1,8 @@
 package com.vcard.vchat.utils
 
+import android.content.Context
+import java.io.IOException
+
 class Utils {
     companion object{
         fun removeUrlSuffix(param: String?): String?{
@@ -7,6 +10,17 @@ class Utils {
                 return null
             }
             return param.removeSuffix(Constants.URL_SUFFIX)
+        }
+
+        fun getJsonDataFromAsset(context: Context, fileName: String): String {
+            val jsonString: String
+            try {
+                jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+            } catch (ioException: IOException) {
+                ioException.printStackTrace()
+                return ""
+            }
+            return jsonString
         }
     }
 }
